26edd9b7dd59c44e695d8c18528221291d0b27db
Hammad Khan <ha39khan@uwaterloo.ca>
2023-03-22 22:35:52 -0400

    #Issues 19 and 20 - Add search and sort functionalities

--- application/src/main/kotlin/net/codebot/application/NoteView.kt
+++ application/src/main/kotlin/net/codebot/application/NoteView.kt
@@ -2,9 +2,47 @@ package net.codebot.application
 import javafx.scene.layout.VBox
 
 class NoteView(private val model: Model): VBox(), IView {
+    private var notelist : ArrayList<Note> = ArrayList()
+    fun sortList(sortVal: String) {
+        if (sortVal == "Title") {
+            notelist.sortBy {
+                it.getTitle().lowercase()
+            }
+        }
+        else {
+            notelist.sortByDescending {
+                it.getTime()
+            }
+        }
+    }
+
+    fun groupList(groupVal: Boolean) {
+      if (groupVal) {
+          notelist.sortBy {
+              it.getGroup()
+          }
+      } else {
+          sortList(model.defaultsort)
+      }
+    }
+
+    fun searchlist(searchVal:String) {
+        if (searchVal != "") {
+            println("hi: $searchVal")
+            notelist = ArrayList( notelist.filter {
+                it.getTitle().startsWith(searchVal)
+            })
+         //  notelist =  notelist.forEach{ println(it.getTitle())}
+        }
+
+    }
+
     override fun update() {
         this.children.clear()
-        var notelist = model.getNotesList()
+        notelist = model.getNotesList()
+        searchlist(model.searchval)
+        sortList(model.defaultsort)
+        groupList(model.defaultgroup)
         notelist.forEach {
             this.children.add(it.getBox())
         }
