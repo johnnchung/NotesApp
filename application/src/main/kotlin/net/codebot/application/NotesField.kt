a2d9ffac5d7ff3d7c057ffba56e2d28cb6a03e80
Hammad Khan <ha39khan@uwaterloo.ca>
2023-03-23 18:07:55 -0400

    Issue #21: Add keyboard shortcuts

--- application/src/main/kotlin/net/codebot/application/NotesField.kt
+++ application/src/main/kotlin/net/codebot/application/NotesField.kt
@@ -1,9 +1,17 @@
 package net.codebot.application
+import javafx.scene.input.KeyCode
 import javafx.scene.web.HTMLEditor
 
 class NotesField(private val model: Model, private val title: String) : HTMLEditor(), IView {
     init {
         model.createView(this)
+
+        // allows the user to use CTRL + S to save the note content
+        this.setOnKeyPressed { event ->
+            if (event.isShortcutDown && event.code == KeyCode.S) {
+                model.saveNotes()
+            }
+        }
     }
     override fun update() {
         if(model.savedNotes) {
