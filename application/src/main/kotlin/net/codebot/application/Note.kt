adfa1f2abab3a210cb2f037475e97d87f6d35f52
John Chung <johnchung03190319@gmail.com>
2023-03-25 14:12:09 -0400

    Make changes to icons and fix functionality of updating title and group fields

--- application/src/main/kotlin/net/codebot/application/Note.kt
+++ application/src/main/kotlin/net/codebot/application/Note.kt
@@ -4,16 +4,17 @@ import javafx.geometry.Insets
 import javafx.geometry.Pos
 import javafx.scene.control.Label
 import javafx.scene.control.TextField
+import javafx.scene.input.KeyCode
 import javafx.scene.input.MouseEvent
 import javafx.scene.layout.*
 import javafx.scene.layout.HBox.setHgrow
 import javafx.scene.paint.Color
 import javafx.scene.text.Font
 import javafx.scene.text.FontWeight
-import java.time.LocalDateTime
+import org.controlsfx.glyphfont.FontAwesome
 import org.controlsfx.glyphfont.Glyph
-import org.controlsfx.glyphfont.FontAwesome;
 import org.controlsfx.glyphfont.GlyphFontRegistry
+import java.time.LocalDateTime
 import java.util.*
 
 // This class is how we create a note instance that goes into  the model's notesList and NotesMap.
@@ -67,6 +68,10 @@ class Note(private val model: Model, title: String, group: String, body: String,
         fontSize = 22.5
         padding = Insets(10.0, 10.0, 0.0, 0.0)
     }
+    private val openIcon: Glyph = GlyphFontRegistry.font("FontAwesome").create(FontAwesome.Glyph.EDIT).apply {
+        fontSize = 22.5
+        padding = Insets(0.0, 10.0, 10.0, 0.0)
+    }
 
     /* This is our title field where the title of the note is displayed.
         If you edit this field, it will enable the update button.
@@ -77,19 +82,7 @@ class Note(private val model: Model, title: String, group: String, body: String,
         border = Border.EMPTY
         background = null
         maxWidth = 130.0
-        val list = model.getNotesMap()
-        textProperty().addListener { _, curVal, newVal ->
-            if(newVal.isNotEmpty()) {
-                for (notes in list) {
-                    // We look for the old title in our map, and set the title to update
-                    if (curVal == notes.key) {
-                        oldTitle = curVal
-                    }
-                }
-                newTitle = newVal
-            }
-            model.updateNote(oldTitle, newTitle, groupField.text)
-        }
+        isEditable = false
     }
 
     /* This is our group field where the group of the note is displayed.
@@ -101,13 +94,6 @@ class Note(private val model: Model, title: String, group: String, body: String,
         border = Border.EMPTY
         maxWidth = 75.0
         background = null
-        textProperty().addListener { _, currVal, newVal ->
-            if(newVal.isNotEmpty()) {
-                oldGroup = currVal
-                newGroup = newVal
-            }
-        }
-        model.updateNote(oldTitle, newTitle, newGroup)
     }
 
     /* This is our bodyText field where the body of the note is displayed.
@@ -115,8 +101,22 @@ class Note(private val model: Model, title: String, group: String, body: String,
         NOTE: Currently, we have a max character limit of 50. The text will be truncated if it's length >= 50.
      */
     val bodyDisplay = Label().apply {
-        text = pureText.ifEmpty {
-            "New Note: Please edit"
+        if(pureText.isEmpty()) {
+            text = "New Note: Please edit"
+        } else {
+            // We will take the substring up to the 3rd word to display on the front
+            var count = 0
+            var endIndex = 0
+            for (i in pureText.indices) {
+                if (pureText[i] == ' ') {
+                    count++
+                    if (count == 3 || i == pureText.length - 1) {
+                        endIndex = i
+                        break
+                    }
+                }
+            }
+            text = pureText.substring(0, endIndex) + "...";
         }
         padding = Insets(0.0, 0.0, 0.0, 15.0)
         setHgrow(this, Priority.ALWAYS)
@@ -133,12 +133,13 @@ class Note(private val model: Model, title: String, group: String, body: String,
     private val combineTitleGroup = VBox(titleFieldBlock, groupFieldBlock, bodyDisplayBlock).apply {
         alignment = Pos.CENTER_LEFT
     }
-    private val deleteBlock = HBox(trashIcon).apply {
+    private val deleteOpenBlock = VBox(trashIcon, openIcon).apply {
+        spacing = 35.0
         padding = Insets(5.0)
     }
 
     // This joins all the buttons and text fields into one HBox to display.
-    private val block = HBox(combineTitleGroup, deleteBlock).apply {
+    private val block = HBox(combineTitleGroup, deleteOpenBlock).apply {
         setHgrow(this, Priority.ALWAYS)
         setHgrow(combineTitleGroup, Priority.ALWAYS)
         maxWidth = Double.MAX_VALUE
@@ -147,17 +148,58 @@ class Note(private val model: Model, title: String, group: String, body: String,
         val color = Color.rgb(random.nextInt(96) + 160, random.nextInt(96) + 160, random.nextInt(96) + 160)
         background = Background(BackgroundFill(color, CornerRadii(15.0), Insets(10.0)))
         border = Border(BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii(25.0), BorderWidths(2.0)))
-        setOnMouseClicked {
-            model.updateNotesPage(newTitle, newGroup, pureText)
-        }
     }
 
     init {
-        /* This event handler for the delete button will delete the note instance
+        /* This event handler for the delete will delete the note instance
            for which the delete button was clicked.
         */
         trashIcon.addEventHandler(MouseEvent.MOUSE_CLICKED) {
             model.deleteNote(newTitle)
+        }
+        openIcon.addEventHandler(MouseEvent.MOUSE_CLICKED) {
+            model.updateNotesPage(newTitle, newGroup, pureText)
+        }
+        titleEditIcon.addEventHandler(MouseEvent.MOUSE_CLICKED) {
+            titleField.apply {
+                isEditable = true
+                val list = model.getNotesMap()
+                textProperty().addListener { _, curVal, newVal ->
+                    if(newVal.isNotEmpty()) {
+                        for (notes in list) {
+                            // We look for the old title in our map, and set the title to update
+                            if (curVal == notes.key) {
+                                oldTitle = curVal
+                            }
+                        }
+                        newTitle = newVal
+                    }
+                    model.updateNote(oldTitle, newTitle, groupField.text)
+                }
+                // Only edit the title field again if the user hits enter
+                setOnKeyPressed { event ->
+                    if (event.code == KeyCode.ENTER) {
+                        isEditable = false
+                    }
+                }
+            }
+        }
+        groupEditIcon.addEventHandler(MouseEvent.MOUSE_CLICKED) {
+            groupField.apply {
+                textProperty().addListener { _, currVal, newVal ->
+                    if(newVal.isNotEmpty()) {
+                        oldGroup = currVal
+                        newGroup = newVal
+                    }
+                }
+                model.updateNote(oldTitle, newTitle, newGroup)
+                // Only edit the group field again if the user hits enter
+                setOnKeyPressed { event ->
+                    if (event.code == KeyCode.ENTER) {
+                        isEditable = false
+                    }
+                }
+            }
         }
     }
 }
